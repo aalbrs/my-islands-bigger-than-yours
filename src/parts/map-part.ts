d68681ae06c670d8321ed5b1b0472b5898ef7c3d
David Aalbers <david.aalbers@gmail.com>
2023-11-24 12:49:48 +1300

    Update basemap again

--- src/parts/map-part.ts
+++ src/parts/map-part.ts
@@ -14,7 +14,7 @@ export interface MapPart {
 export function setupMap(element: HTMLDivElement, searchDomNode: HTMLDivElement, searchPlaceholder: string): MapPart {
 
     var map = new Map({
-        basemap: "topo-vector", // "topo-3d" uses different licensing
+        basemap: "arcgis-topographic", // also "topo-vector", "topo-3d", but may use different licensing
         
         // A ground preset containing a single elevation layer, sourced from
         // https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer
